3098d475d0611c27ea335226a0b19516e2e5b805
Chris Huckins <huckinsc@amazon.com>
2021-02-15 10:17:18 -0500

    Added rankValues to the card ranks.

--- src/com/java6casino/videopoker/Rank.java
+++ src/com/java6casino/videopoker/Rank.java
@@ -1,16 +1,22 @@
 package com.java6casino.videopoker;
 
 enum Rank {
-    ACE(" A"), ONE(" 1"), TWO(" 2"), THREE(" 3"), FOUR(" 4"), FIVE(" 5"), SIX(" 6"), SEVEN(" 7"), EIGHT(" 8"),
-    NINE(" 9"), TEN("10"), JACK(" J"), QUEEN(" Q"), KING(" K");
+    ACE(" A", 1), TWO(" 2", 2), THREE(" 3", 3), FOUR(" 4", 4), FIVE(" 5", 5), SIX(" 6", 6), SEVEN(" 7", 7), EIGHT(" 8", 8),
+    NINE(" 9", 9), TEN("10", 10), JACK(" J", 11), QUEEN(" Q", 12), KING(" K", 13);
 
     private final String rankCharacter;
+    private final int rankValue;
 
-    Rank(String rankCharacter){
+    Rank(String rankCharacter, int rankValue){
         this.rankCharacter = rankCharacter;
+        this.rankValue = rankValue;
     }
 
     public String getRankCharacter() {
         return rankCharacter;
+    }
+
+    public int getRankValue() {
+        return rankValue;
     }
 }
