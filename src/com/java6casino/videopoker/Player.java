a7a171b5525956c58d09e8e68431b7867ac14db9
Chris Huckins <huckinsc@amazon.com>
2021-02-17 07:21:12 -0500

    Modified the PLayer class to hold cards and to pay bets

--- src/com/java6casino/videopoker/Player.java
+++ src/com/java6casino/videopoker/Player.java
@@ -1,62 +1,78 @@
 package com.java6casino.videopoker;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 class Player {
+    private final int HAND_SIZE = 5;
+
     //FIELDS
     String name;
     private int credits = 100;
     int bet;
+    List<Boolean> heldCards = new ArrayList<>();
     private Scanner scanner;
-
 
 
     //CONSTRUCTOR
 
-    public Player(String name){
+    public Player(String name) {
         setName(name);
-
         Hand playerHand = new Hand();
+        unholdAllCards();
 
 
     }
 
+    // Business Methods
 
-    public void placeBet() throws InputMismatchException{
+    void unholdAllCards() {
+        heldCards.clear();
+        for (int i = 0; i < HAND_SIZE; i++) {
+            heldCards.add(Boolean.FALSE);
+        }
+    }
+
+    public void placeBet() throws InputMismatchException {
         System.out.println(" Please place your bet");  // request the player to enter bet amount
 
-        try{
+        try {
             bet = scanner.nextInt();
-              // check to so see if player can cover bet
+            // check to so see if player can cover bet
             if (bet > credits) {
                 System.out.println("You don't have enough to cover. please try again"); //
                 placeBet();
             }
-            if (bet <= 0){
+            if (bet <= 0) {
                 System.out.println("Please place a valid bet");
                 placeBet();
-            }
-            else{
+            } else {
                 credits -= bet;
             }
-            }
-          // catch the exception when player does not use numbers for bet
-        catch (InputMismatchException e){
+        }
+        // catch the exception when player does not use numbers for bet
+        catch (InputMismatchException e) {
             System.out.println("Please input numbers only only");
             placeBet();
         }
 
+    }
+
+    void payBet(int betAmount){
+        if (betAmount <= getCredits() ) {
+            setCredits(getCredits() - betAmount);
         }
-
-
-    void holdCards() {
-
-
     }
 
 
-       //ACCESSORS
+    void holdCard(int cardPosition) {
+        heldCards.set(cardPosition, !heldCards.get(cardPosition));
+    }
+
+
+    //ACCESSORS
 
     public int getCredits() {
         return credits;
@@ -70,5 +86,12 @@ class Player {
         this.name = name;
     }
 
+    public List<Boolean> getHeldCards() {
+        return heldCards;
+    }
 
+    // Setters
+    void setCredits(int newCreditAmount) {
+        credits = newCreditAmount;
+    }
 }//End of Class
